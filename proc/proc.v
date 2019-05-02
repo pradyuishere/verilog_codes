a84b154bf4b8d109e60d7bcb86ae9d5929913b9a
pradyuishere <you@yourpublicemail.example.com>
2019-05-03 01:43:41 +0530

    changed proc to 9 bit result and added comments

--- proc/proc.v
+++ proc/proc.v
@@ -1,32 +1,60 @@
+/*
+
+**********Module Proc.v*************
+Functions executed :
+  1 - AND
+  2 - ADD
+  3 - FETCH
+
+  The third operation takes in the argument last 8 bit operand in the
+  instruction input to check the memory location and return the value
+  found and also copies the same into the destination register as well.
+*/
 module proc(
   input clk,
   input [31:0] instr,
-  output [7:0] res
+  output [8:0] res
   );
+  
+  //Storage array to store the computed results.
+  //Each of the elements in this array are 9 bits in size in order to accomodate
+  // the 8 bit sum and 1 bit carry formed by adding two 8 bit numbers
+  reg [8:0] storage [7:0];
 
-  reg [7:0] storage [7:0];
-
+  //Control signals to control the muxes in the execute stage
   reg instr_and, instr_add, instr_fetch;
+
+  //Data to be passed onto the execute stage along with the control signals
   reg [23:0] data_exec;
+  
   //instr_decode
+  //Decode the first 8 bits of the decode stage to generate the control signals
   always@(posedge clk)
   begin
-    $display("instr : %d", instr[31:24]);
+    //Initialize the values to zero 
     instr_add <= 0;
     instr_and <= 0;
     instr_fetch <= 0;
+    //Update only the signal corresponding to the instruction
     if(instr[31:24] == 0)
       instr_and <= 1;
     else if(instr[31:24] == 1)
       instr_add <= 1;
     else if(instr[31:24] == 2)
       instr_fetch <= 1;
+    //Pass the memory location and operands from the instruction to the execute stage
     data_exec <= instr[23:0];
   end
 
-  //instr_execute
-  reg [7:0] data_write;
+  //Data to be written to the storage location in write_back stage
+  reg [8:0] data_write;
+  reg temp_carry;
+  reg [7:0] temp_sum;
+  //Location registers from the instr to be passed to the write_back stage
   reg [7:0] location_write;
+  
+  //instr_execute
+  //Executes the operation based on the control signals
   always@(posedge clk)
   begin
     if(instr_add == 1)
@@ -37,14 +65,21 @@ module proc(
     begin
       data_write <= data_exec[7:0] & data_exec [15:8];
     end
+    //This operation instr_fetch fetches the data stored in the storage location requested
     else if(instr_fetch == 1)
     begin
       data_write <= storage[data_exec[7:0]];
     end
 
+    //Pass the location to write back to the next stage
     location_write <= data_exec [23:16];
   end
 
+
+  //write_back stage
+  //This stage takes the result and storage location from the previous stage
+  // stores them in the location requested. Also, it assigns the result to
+  // res output of the module.
   always@(posedge clk)
   begin
     storage[location_write] <= data_write;
--- proc/tb_proc.v
+++ proc/tb_proc.v
@@ -1,29 +1,49 @@
 module tb_proc();
-
+  //Variable declaration
   reg clk;
   reg [31:0] instr;
-  wire [7:0] res;
+
+  //****Please note that the result is of size 9 bits in order to accomodate the carry bit*****
+  wire [8:0] res;
+  //proc module instantiation
   proc p1(clk, instr, res);
 
-  reg [7:0] temp [0:3];
+  reg [7:0] temp [0:3]; //An array of 4 elements which together constitute the 32 bit instr
 
   initial begin
     clk = 0;
+    //Dump the values of all the variables into the dumpfile
     $dumpfile("a.vcd");
     $dumpvars;
+    /*For the following instructions,
+    	temp[3] = opcode;
+    	temp[2] = data_destination;
+    	temp[1] = operand_1;
+    	temp[0] = operand_2;
+    */ 
+    //Instr_1
+    //Instr to "and" the operands
     temp[3] = 0; temp[2] = 3;temp[1] = 57;temp[0] = 100;
     @(posedge clk) instr <= {temp[3], temp[2], temp[1], temp[0]};
-    temp[3] = 1; temp[2] = 4;temp[1] = 57;temp[0] = 100;
+    //Instr_2
+    //Instr to "add" the operands
+    temp[3] = 1; temp[2] = 4;temp[1] = 255;temp[0] = 255;
     @(posedge clk) instr <= {temp[3], temp[2], temp[1], temp[0]};
+    //Instr_3
+    //Instr to display the data in the memory location temp[0]
     temp[3] = 2; temp[2] = 5;temp[1] = 57;temp[0] = 3;
     @(posedge clk) instr <= {temp[3], temp[2], temp[1], temp[0]};
+    //Instr_4
+    //Instr to display the data in the memory location temp[0]
     temp[3] = 2; temp[2] = 6;temp[1] = 57;temp[0] = 4;
     @(posedge clk) instr <= {temp[3], temp[2], temp[1], temp[0]};
+    //Finish the execution after 100 clock units
     #100 $finish;
   end
 
   always
   begin
+  	//Alternate the clock after every 5 clock units, effective clock delay is 10 units
     #5 clk = ~clk;
   end
 
